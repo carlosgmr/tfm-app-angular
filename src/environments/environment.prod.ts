cccff299796804fa059fd6bd560486b80ae8bc02
Carlos Molina <cmolinaronceros@gmail.com>
2018-09-01 01:12:26 +0200

    Actualizando archivos de configuración

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  app: {
+    name: 'TFM App Angular',
+    name1: 'App',
+    name2: 'Angular',
+    version: '1.0.0',
+    copyright: 'Carlos Giovanni Molina Ronceros',
+    copyrightUrl: 'https://github.com/carlosgmr'
+  },
+  api: {
+    url: 'http://192.168.1.61:8957/'
+  }
 };
